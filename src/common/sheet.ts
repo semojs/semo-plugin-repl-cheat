5234ac9f5f5308c3bfe1ec0a320ffb00854b7ea8
Richard Yu <vipzhicheng@gmail.com>
2020-08-26 19:55:08 +0800

    fix: use core console reader

--- src/common/sheet.ts
+++ src/common/sheet.ts
@@ -1,8 +1,5 @@
 import got from 'got'
 
-import fs from 'fs'
-import path from 'path'
-import { execSync } from 'child_process'
 import { Utils } from '@semo/core'
 
 export const getSheet = async (input, opts) => {
@@ -14,24 +11,10 @@ export const getSheet = async (input, opts) => {
       }
     })
 
-    if (process.env.HOME) {
-      const tmpPath = path.resolve(process.env.HOME, '.semo/cache/semo-plugin-repl-sheet/', Utils.md5(input))
-      await Utils.fs.ensureDirSync(path.dirname(tmpPath))
-      fs.writeFileSync(tmpPath, response.body)
-      execSync(`cat ${tmpPath} | less -r`, {
-        stdio: [0, 1, 2]
-      })
-
-      // Maybe the file already removed
-      if (fs.existsSync(tmpPath)) {
-        fs.unlinkSync(tmpPath)
-      }
-
-    } else {
-      console.log((response.body))
-
-    }
-
+    Utils.consoleReader(response.body, {
+      identifier: input,
+      plugin: 'semo-plugin-repl-sheet'
+    })
 
   } catch (e) {
     console.error(e.message)
