5c45ea9cc50f756edf0bd5d6a2f6d21bcd02cba8
Richard Yu <vipzhicheng@gmail.com>
2020-08-26 17:38:37 +0800

    feat: add less mode and cheat alias

--- src/common/sheet.ts
+++ src/common/sheet.ts
@@ -1,5 +1,10 @@
 import got from 'got'
 
+import fs from 'fs'
+import path from 'path'
+import { execSync } from 'child_process'
+import { Utils } from '@semo/core'
+
 export const getSheet = async (input, opts) => {
   try {
     const url = `https://cheat.sh/${input}`
@@ -8,7 +13,25 @@ export const getSheet = async (input, opts) => {
         'User-Agent': 'curl'
       }
     })
-    console.log((response.body))
+
+    if (process.env.HOME) {
+      const tmpPath = path.resolve(process.env.HOME, '.semo/cache/semo-plugin-repl-sheet/', Utils.md5(input))
+      await Utils.fs.ensureDirSync(path.dirname(tmpPath))
+      fs.writeFileSync(tmpPath, response.body)
+      execSync(`cat ${tmpPath} | less -r`, {
+        stdio: [0, 1, 2]
+      })
+
+      // Maybe the file already removed
+      if (fs.existsSync(tmpPath)) {
+        fs.unlinkSync(tmpPath)
+      }
+
+    } else {
+      console.log((response.body))
+
+    }
+
 
   } catch (e) {
     console.error(e.message)
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,26 +4,28 @@ import { getSheet } from '../common/sheet'
 export = (Utils) => {
   return {
     hook_repl_command: new Utils.Hook('semo', () => {
-      return {
-        sheet: {
-          help: 'Get commands help information from cheat.sh',
-          async action(input) {
-            if (!input) {
-              Utils.warn('keyword is required')
-            } else {
-              // @ts-ignore
-              this.clearBufferedCommand()
-              let opts = Utils.yParser(input)
-              let keyword = opts._.join(' ')
+      let command = {
+        help: 'Get commands help information from cheat.sh',
+        async action(input) {
+          if (!input) {
+            Utils.warn('keyword is required')
+          } else {
+            // @ts-ignore
+            this.clearBufferedCommand()
+            let opts = Utils.yParser(input)
+            let keyword = opts._.join(' ')
 
-              await getSheet(keyword, opts)
+            await getSheet(keyword, opts)
 
-               // @ts-ignore
-              this.displayPrompt()
-            }
+             // @ts-ignore
+            this.displayPrompt()
           }
-
         }
+
+      }
+      return {
+        sheet: command,
+        cheat: command,
       }
     })
   }
